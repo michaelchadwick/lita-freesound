d060664b0f301091513cbcfe0ca3b868d9ff6266
Michael Chadwick <michael.chadwick@gmail.com>
2016-03-08 11:29:33 -0800

    first attempt at querying freesound for results

--- lib/lita/handlers/freesound.rb
+++ lib/lita/handlers/freesound.rb
@@ -1,7 +1,43 @@
 module Lita
   module Handlers
     class Freesound < Handler
-      # insert handler code here
+      URL = "http://freesound.org/apiv2/search/test/"
+      NO_RESULTS = 'Freesound has no results for that.'
+
+      config :api_key, type: String, required: true
+
+      route(/freesound|fs/i, :get_sound, command: true, help: {
+        "freesound SEARCH_TERM" => "Return the first Freesound search result for SEARCH_TERM"
+      })
+
+      def get_sound(response)
+        query = response.matches[0][0]
+
+        http_response = http.get(
+          "#{URL}?query=#{query}&token=#{Lita.config.handlers.freesound.api_key}",
+          limit: 1
+        )
+
+        case http_response.status
+        when 200
+          data = MultiJson.load(http_response.body)
+
+          result = data["responseData"]["results"]
+
+          if result
+            response.reply result
+            )
+          else
+            response.reply("No search results for query: #{query}")
+          end
+        when 404
+          NO_RESULTS
+        else
+          Lita.logger.warn(
+            "Freesound did not like the query: #{query}"
+          )
+        end
+      end
 
       Lita.register_handler(self)
     end
